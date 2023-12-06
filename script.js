b9f7d9667b0984871ecfd8668e4ccb9d6776707e
Alvin Hsu <a1hsu@ucsd.edu>
2023-12-05 17:38:39 -0800

    part 1

--- script.js
+++ script.js
@@ -1,123 +1,34 @@
 window.onload = () => {
-    let textarea = document.getElementById("comments");
-    let wordCount = document.getElementById("info_comments");
-    let errorCount = document.getElementById("error_comments")
-    const isDarkMode = localStorage.getItem("isDarkMode");
-    let button = document.getElementById("toggle_button")
-    button.textContent = isDarkMode && isDarkMode=="true" ? "🌝" : "🌚";
-    console.log(isDarkMode);
-    if (isDarkMode == "true") {
-        toggleDark();
-    }
-    const form_error = new Set();
+    let rating = document.getElementById("rating");
+    let maxStars = rating.getAttribute("max");
+    let form = document.getElementById("my_form")
 
-    let mxLength = 200
-    let mnLength = 5
+    //hide the html wrapper when using js
+    let no_js_wrapper = document.getElementById("no_js_wrapper");
+    no_js_wrapper.style.visibility = "hidden";
 
-    let printableRegex = /[ -~]/;
-    if (wordCount) wordCount.innerHTML = textarea.value.length + "/" + mxLength;
-    textarea && textarea.addEventListener("input", function () {
-        if (textarea.value.length > mxLength){
-            textarea.value = textarea.value.substr(0, mxLength);
-            textarea.value = textarea.value
-            errorCount.innerHTML = "Attempted max length error";
-            errorCount.classList.add("fade-out");
-            form_error.add(errorCount.innerHTML);
-            setTimeout(() => {
-                errorCount.innerHTML = "";
-                errorCount.classList.remove("fade-out");
-            }, 2000);
-        }
-
-        if (textarea.value && !printableRegex.test(textarea.value)){
-            textarea.value = textarea.value.replace(/[^ -~]+/g, '');
-            errorCount.innerHTML = "Attempted invalid comment character";
-            errorCount.classList.add("fade-out");
-            form_error.add(errorCount.innerHTML);
-            setTimeout(() => {
-                errorCount.innerHTML = "";
-                errorCount.classList.remove("fade-out");
-            }, 2000);
-        }
-
-        wordCount.innerHTML = textarea.value.length + "/" + mxLength;
-        wordCount.style.color = textarea.value.length > mxLength-25 ? "red" : "black"
-    });
-
-    let nameInput = document.getElementById("name");
-    const nameRegex = /^[A-Za-z\s]+$/;
-    let errorName = document.getElementById("error_name")
-    nameInput && nameInput.addEventListener("input", function(){
-        let okName = nameRegex.test(nameInput.value);
-        if (!okName){
-            nameInput.value = nameInput.value.replace(/[^A-Za-z\s]+/g, '');
-            // errorName.innerHTML = "Attempted invalid name character";
-            errorName.setCustomValidity("TEST");
-            errorName.classList.add("fade-out");
-            form_error.add(errorName.innerHTML);
-            setTimeout(() => {
-                errorName.innerHTML = "";
-                errorName.classList.remove("fade-out");
-            }, 2000);
-        }
-    });
-      
-
-    function validateForm(){
-        let ok = true;
-        var text = textarea.value;
-        // validate end of text for comments
-        if (text.length < mnLength){
-            errorCount.innerHTML = "Comments length smaller than min length required";
-            errorCount.classList.add("fade-out");
-            form_error.add(errorCount.innerHTML);
-
-            setTimeout(() => {
-                errorCount.innerHTML = "";
-                errorCount.classList.remove("fade-out");
-            }, 2000);
-            ok = false;
-        }
-
-        //validate email
-        let email = document.getElementById("email").value;
-        const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        let errorEmail = document.getElementById("error_email")
-        if (!emailRegex.test(email)){
-            errorEmail.innerHTML = "Invalid email";
-            errorEmail.classList.add("fade-out");
-            form_error.add(errorEmail.innerHTML);
-
-            setTimeout(() => {
-                errorEmail.innerHTML = "";
-                errorEmail.classList.remove("fade-out");
-            }, 2000);
-            ok = false;
-        }
-        return ok;
+    //sent by JS
+    let sentBy = document.getElementById("sentBy");
+    sentBy.setAttribute("value", "JS");
+    let js_wrapper = document.getElementById("js_wrapper");
+    //add in max number of stars
+    for (let i = 1; i <= maxStars; i++){
+        let newChild = document.createElement("div");
+        newChild.innerHTML = "&#9733;"
+        newChild.setAttribute("value", i);
+        js_wrapper.appendChild(newChild);
     }
 
-    let myForm = document.getElementById("my_form");
-    myForm && myForm.addEventListener("submit", function(event){
-        if (!validateForm()){
-            event.preventDefault();
-        }
-        else {
-            const newInput = document.createElement("input");
-            newInput.type = "hidden";
-            newInput.name = "form_error";
-            newInput.value = JSON.stringify(Array.from(form_error));
-            myForm.appendChild(newInput);
-        }
+    js_wrapper.addEventListener("click", function(e) {
+        let ratingChosen = e.target;
+        rating.setAttribute("value", ratingChosen.getAttribute("value"));
+        fetch('https://httpbin.org/post', {
+            method: 'POST',
+            body: new FormData(form),
+            headers: {'X-Sent-By': 'JS'}
+        }).then(response => response.json()).then(data => {
+            //log the response data
+            console.log(data);
+        })
     });
-}
-
-function toggleDark(){
-    document.body.classList.toggle("dark-mode");
-    document.getElementById("toc_container") && document.getElementById("toc_container").classList.toggle("dark-mode");
-    let isDarkMode = document.body.classList.contains("dark-mode");
-    let button = document.getElementById("toggle_button")
-    button.textContent = isDarkMode ? "🌝" : "🌚";
-    document.getElementsByTagName("pre").item(0) && document.getElementsByTagName("pre").item(0).classList.toggle("dark-mode")
-    localStorage.setItem('isDarkMode', isDarkMode);
 }
